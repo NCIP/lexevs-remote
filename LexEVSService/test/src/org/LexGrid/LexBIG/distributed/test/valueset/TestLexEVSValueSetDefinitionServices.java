88382bccec4073948219a756743fdcb302f7063d
Sridhar Yadav <Yadav.Sridhar@mayo.edu>
2011-02-24 20:52:56 +0000

    Export Value Set Resolution for supplied Value Set Definition Object. GFroge # 30262

--- LexEVSService/test/src/org/LexGrid/LexBIG/distributed/test/valueset/TestLexEVSValueSetDefinitionServices.java
+++ LexEVSService/test/src/org/LexGrid/LexBIG/distributed/test/valueset/TestLexEVSValueSetDefinitionServices.java
@@ -22,15 +22,35 @@
  */
 package org.LexGrid.LexBIG.distributed.test.valueset;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.LexGrid.LexBIG.DataModel.Collections.CodingSchemeRenderingList;
+import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
+import org.LexGrid.LexBIG.DataModel.InterfaceElements.CodingSchemeRendering;
 import org.LexGrid.LexBIG.Exceptions.LBException;
+import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
 import org.LexGrid.LexBIG.Utility.Iterators.ResolvedConceptReferencesIterator;
 import org.LexGrid.LexBIG.testUtil.LexEVSServiceHolder;
 import org.LexGrid.LexBIG.testUtil.ServiceTestCase;
+import org.LexGrid.util.PrintUtility;
 import org.LexGrid.valueSets.DefinitionEntry;
 import org.LexGrid.valueSets.EntityReference;
 import org.LexGrid.valueSets.ValueSetDefinition;
+import org.LexGrid.valueSets.ValueSetDefinitionReference;
 import org.LexGrid.valueSets.types.DefinitionOperator;
 import org.lexgrid.valuesets.LexEVSValueSetDefinitionServices;
+import org.lexgrid.valuesets.dto.ResolvedValueSetDefinition;
+import org.lexgrid.valuesets.helper.LexEVSPipedReader;
+
+import com.healthmarketscience.rmiio.SerializableInputStream;
 
 /**
  * The Class TestLexEVSValueSetDefinitionServices.
@@ -45,31 +65,123 @@ public class TestLexEVSValueSetDefinitionServices extends ServiceTestCase
         return testID;
     }
 
-    public void testResolve() throws LBException {
+    public void testSupportedCS() throws LBInvocationException {
+    	CodingSchemeRenderingList csrList = LexEVSServiceHolder.instance().getLexEVSAppService().getSupportedCodingSchemes();
+    	for (CodingSchemeRendering csr : csrList.getCodingSchemeRendering()){
+    		System.out.println(csr.getCodingSchemeSummary().getFormalName());
+    	}
+    }
+    
+    public void testListVSD(){
+    	List<String> uris = LexEVSServiceHolder.instance().getLexEVSAppService().getLexEVSValueSetDefinitionServices().listValueSetDefinitionURIs();
+    	
+    	for (String uri : uris)
+    	{
+    		System.out.println("VSDs loaded : " + uri);
+    	}
+    }
+    
+    public void testExportVSResolutionByVSDObjectToWriter() throws LBException, URISyntaxException, IOException{
+		
+		ValueSetDefinition vsd = new ValueSetDefinition();
+		vsd.setValueSetDefinitionURI("SRITEST:AUTO:VSDREF_GM_IMMI_NODE_AND_FORD");
+		vsd.setValueSetDefinitionName("VSDREF_GM_IMMI_NODE_AND_FORD");
+		vsd.setDefaultCodingScheme("Automobiles");
+		vsd.setConceptDomain("Autos");
+		
+		DefinitionEntry de = new DefinitionEntry();
+		de.setRuleOrder(1L);
+		de.setOperator(DefinitionOperator.OR);
+		
+		vsd.addDefinitionEntry(de);
+		
+		ValueSetDefinitionReference vsdRef = new ValueSetDefinitionReference();
+		vsdRef.setValueSetDefinitionURI("SRITEST:AUTO:GM_AND_IMMI_NODE");
+		de.setValueSetDefinitionReference(vsdRef);
+		
+		de = new DefinitionEntry();
+		de.setRuleOrder(2L);
+		de.setOperator(DefinitionOperator.OR);
+		
+		EntityReference entityRef = new EntityReference();
+		entityRef.setEntityCode("Explorer");
+		entityRef.setEntityCodeNamespace("Automobiles");
+		entityRef.setLeafOnly(false);
+		entityRef.setTransitiveClosure(false);
+		de.setEntityReference(entityRef);
+		
+		vsd.addDefinitionEntry(de);
+		
+		// Start the value set resolution export
+		InputStream reader =  LexEVSServiceHolder.instance().getLexEVSAppService().getLexEVSValueSetDefinitionServices().exportValueSetResolution(vsd, null, null, null, false);
+		
+		if (reader != null) {
+			StringBuffer buf = new StringBuffer(); 
+	        try { 
+	            for(int c = reader.read(); c != -1; c = reader.read()) { 
+	                buf.append((char)c); 
+	            } 
+	            System.out.println(buf.toString()); 
+	        } catch(IOException e) { 
+	            throw e; 
+	        } finally { 
+	            try { 
+	                reader.close(); 
+	            } catch(Exception e) { 
+	                // ignored 
+	            } 
+	        } 
+		}
+	}
+    
+    public void testResolve() throws LBException, URISyntaxException {
 
     	LexEVSValueSetDefinitionServices vds = 
     		LexEVSServiceHolder.instance().getLexEVSAppService().getLexEVSValueSetDefinitionServices();
 
-    	ValueSetDefinition vsd = new ValueSetDefinition();
-    	vsd.setValueSetDefinitionName("vsName");
-    	vsd.setValueSetDefinitionURI("vsUri");
-    	vsd.setDefaultCodingScheme(ServiceTestCase.THES_SCHEME);
-
-    	DefinitionEntry entry1 = new DefinitionEntry();
-    	entry1.setRuleOrder(new java.lang.Long(0));
-    	entry1.setOperator(DefinitionOperator.OR);
-
-    	EntityReference entity = new EntityReference();
-    	entity.setEntityCode("C12727");
-    	entry1.setEntityReference(entity);
-
-    	vsd.addDefinitionEntry(entry1);
-
-    	ResolvedConceptReferencesIterator itr = 
-    		vds.resolveValueSetDefinition(vsd, null, null, null).getResolvedConceptReferenceIterator();
+//    	ValueSetDefinition vsd = new ValueSetDefinition();
+//    	vsd.setValueSetDefinitionName("vsName");
+//    	vsd.setValueSetDefinitionURI("vsUri");
+//    	vsd.setDefaultCodingScheme(ServiceTestCase.THES_SCHEME);
+//
+//    	DefinitionEntry entry1 = new DefinitionEntry();
+//    	entry1.setRuleOrder(new java.lang.Long(0));
+//    	entry1.setOperator(DefinitionOperator.OR);
+//
+//    	EntityReference entity = new EntityReference();
+//    	entity.setEntityCode("C12727");
+//    	entry1.setEntityReference(entity);
+//
+//    	vsd.addDefinitionEntry(entry1);
+//
+//    	ResolvedConceptReferencesIterator itr = 
+//    		vds.resolveValueSetDefinition(vsd, null, null, null).getResolvedConceptReferenceIterator();
+//    	
+//    	assertEquals(1,itr.numberRemaining());
+//
+//    	assertEquals("C12727", itr.next().getEntity().getEntityCode());
     	
-    	assertEquals(1,itr.numberRemaining());
-
-    	assertEquals("C12727", itr.next().getEntity().getEntityCode());
+    	List<String> uris = vds.listValueSetDefinitionURIs();
+    	
+    	for (String uri : uris){
+    		System.out.println("available URI : " + uri);
+    	}
+    	
+    	ResolvedValueSetDefinition rvsd = vds.resolveValueSetDefinition(new URI("SRITEST:AUTO:GM"), null, null, null, null);
+    	
+    	ResolvedConceptReferencesIterator rcrItr = rvsd.getResolvedConceptReferenceIterator();
+    	
+    	while (rcrItr.hasNext())
+    	{
+    		ResolvedConceptReference rcr = rcrItr.next();
+    		System.out.println("code : " + rcr.getCode());
+    	}
+    	uris = vds.listValueSetsWithEntityCode("GM", null, null, null);
+    	
+    	for (String uri : uris){
+    		System.out.println("URI : " + uri);
+    	}
+    	
+    	
     }
 }
