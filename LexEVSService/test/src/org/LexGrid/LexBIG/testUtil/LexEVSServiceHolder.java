922a85871127704d83c7a3ba2703b5629b83abe7
Kevin Peterson <kevin.peterson@mayo.edu>
2013-08-19 13:35:19 -0500

    Updated header files.

--- LexEVSService/test/src/org/LexGrid/LexBIG/testUtil/LexEVSServiceHolder.java
+++ LexEVSService/test/src/org/LexGrid/LexBIG/testUtil/LexEVSServiceHolder.java
@@ -1,22 +1,11 @@
-/*******************************************************************************
- * Copyright: (c) 2004-2009 Mayo Foundation for Medical Education and 
- * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
- * triple-shield Mayo logo are trademarks and service marks of MFMER.
- * 
- * Except as contained in the copyright notice above, or as used to identify 
- * MFMER as the author of this software, the trade names, trademarks, service
- * marks, or product names of the copyright holder shall not be used in
- * advertising, promotion or otherwise in connection with this software without
- * prior written authorization of the copyright holder.
- *   
- * Licensed under the Eclipse Public License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
- *   
- *  		http://www.eclipse.org/legal/epl-v10.html
- * 
- *  		
- *******************************************************************************/
+/*
+* Copyright: (c) Mayo Foundation for Medical Education and
+* Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+* triple-shield Mayo logo are trademarks and service marks of MFMER.
+*
+* Distributed under the OSI-approved BSD 3-Clause License.
+* See http://ncip.github.com/lexevs-remote/LICENSE.txt for details.
+*/
 package org.LexGrid.LexBIG.testUtil;
 
 import org.LexGrid.LexBIG.caCore.interfaces.LexEVSApplicationService;
