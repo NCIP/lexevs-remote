a33f057b96f46b8ef1659af7d134a796bcad4b2f
hsbauer <bauer.scott@mayo.edu>
2019-07-17 14:00:22 -0500

    LEXEVSREM-388. Adjusting externally driven tests and configurtations.  Should have no effect on the CI integration tests

--- LexEVSService/test/src/org/LexGrid/LexBIG/distributed/test/bugs/TestLEXEVS_3465.java
+++ LexEVSService/test/src/org/LexGrid/LexBIG/distributed/test/bugs/TestLEXEVS_3465.java
@@ -6,6 +6,7 @@ import org.LexGrid.LexBIG.Exceptions.LBParameterException;
 import org.LexGrid.LexBIG.Extensions.Generic.MappingExtension;
 import org.LexGrid.LexBIG.Extensions.Generic.MappingExtension.Mapping;
 import org.LexGrid.LexBIG.Extensions.Generic.MappingExtension.Mapping.SearchContext;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet.SearchDesignationOption;
 import org.LexGrid.LexBIG.Utility.Constructors;
 import org.LexGrid.LexBIG.Utility.Iterators.ResolvedConceptReferencesIterator;
 import org.LexGrid.LexBIG.caCore.interfaces.LexEVSApplicationService;
@@ -14,36 +15,39 @@ import org.LexGrid.LexBIG.testUtil.ServiceTestCase;
 import org.junit.Before;
 import org.junit.Test;
 
+import gov.nih.nci.system.client.ApplicationServiceProvider;
+
 public class TestLEXEVS_3465 extends ServiceTestCase {
-	LexEVSApplicationService svc;
 
 	@Before
 	protected void setUp() throws Exception {
 		super.setUp();
-		svc = LexEVSServiceHolder.instance().getLexEVSAppService();
 	}
 
 	@Test
 	public void test() throws LBException {
-
-		MappingExtension mappingExtension = (MappingExtension) svc.getGenericExtension("MappingExtension");
 		
+		LexEVSApplicationService lbs = LexEVSServiceHolder.instance().getLexEVSAppService();
+		MappingExtension mappingExtension = (MappingExtension) lbs.getGenericExtension("MappingExtension");
 		Mapping mapping = mappingExtension.getMapping(
-				ITERATOR_MAPPING_SCHEME, 
-				Constructors.createCodingSchemeVersionOrTagFromVersion(ITERATOR_MAPPING_SCHEME_VERSION), 
-				null);
+				"SNOMEDCT_US_2018_09_01_TO_ICD10_2016", 
+				Constructors.createCodingSchemeVersionOrTagFromVersion("20180901"), null);
 		
-		mapping.restrictToCodes(Constructors.createConceptReferenceList(ITERATOR_MAPPING_SCHEME_CODE), SearchContext.TARGET_CODES);
-		ResolvedConceptReferencesIterator itr = mapping.resolveMapping();
+		mapping = mapping.restrictToMatchingDesignations("Cholera, unspecified", SearchDesignationOption.PREFERRED_ONLY,"LuceneQuery", null, SearchContext.TARGET_CODES);
+
+		ResolvedConceptReferencesIterator itr = mapping.resolveMapping(null);
 		
-//		int count = 0;
-//		while(itr.hasNext()) {
-//			ResolvedConceptReference next = itr.next();
-//			assertNotNull(next);
-//			count++;
-//		}
-		fail("when the code is commented in");
-//		assertEquals(2,count);
+		int count = 0;
+		int numberRemaining = itr.numberRemaining();
+		while(itr.hasNext()) {
+			ResolvedConceptReference next = itr.next();
+			assertNotNull(next);
+			System.out.println(next.getCode());
+			count++;
+			if(count > 5){fail("too many results");}
+		}
+		
+		assertEquals(count, numberRemaining);
 	}
 
 }
